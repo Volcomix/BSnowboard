fc554253dcd68146458d48ebb20d7ac484d44f7f
Volcomix <Volcomix@users.noreply.github.com>
2014-04-27 17:20:23 +0200

    Remove unnecessary raycasts.

--- sources/chars/snowboarder.py
+++ sources/chars/snowboarder.py
@@ -3,21 +3,43 @@ import mathutils
 
 EPSILON = 1.0e-10
 
-align_damping = 5
-align_filter = align_damping / (1.0 + align_damping)
+#Air properties
+air_raydist = 2
+air_aligndamping = 50
 
-def fakie(controller):
+# Ground properties
+ground_raydist = 0.15
+ground_aligndamping = 5
+
+# Air constants
+air_alignfilter = air_aligndamping / (1.0 + air_aligndamping)
+
+# Ground constants
+ground_alignfilter = ground_aligndamping / (1.0 + ground_aligndamping)
+ground_raydist_squared = ground_raydist * ground_raydist
+
+def update(controller):
     snowboarder = controller.owner
+    
     snowboarder['fakie'] = snowboarder.localLinearVelocity.y > 0
-
-def align(controller):
-    snowboarder = controller.owner
+    
     orimat = snowboarder.worldOrientation
     z = orimat.col[2]
-    object, hitpoint, normal = snowboarder.rayCast(snowboarder.worldPosition - z, None, 1, 'ground', 1)
+    direction = snowboarder.worldPosition - z
+    object, hitpoint, normal = snowboarder.rayCast(direction, None, air_raydist, 'ground', 1)
 
-    if normal:
-        normal = align_filter * z + (1.0 - align_filter) * normal
+    if hitpoint:
+        ray = hitpoint - snowboarder.worldPosition
+        dist_squared = ray.length_squared
+        
+        if dist_squared < ground_raydist_squared:
+            snowboarder['onground'] = True
+            filter = ground_alignfilter
+        else:
+            snowboarder['onground'] = False
+            filter = air_alignfilter
+        
+        normal = filter * z + (1.0 - filter) * normal
         
         ori = orimat.col[1]
         if abs(normal.dot(ori)) > 1 - 3 * EPSILON:
@@ -36,3 +58,5 @@ def align(controller):
         orimat.col[0] = x
         orimat.col[1] = y
         orimat.col[2] = z
+    else:
+        snowboarder['onground'] = False
